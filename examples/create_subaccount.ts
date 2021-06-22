22cabaa3f0a8c64a46bf70a101a941207111ba46
xuanlc113 <xuan.lc113@gmail.com>
2021-06-22 17:18:17 +0800

    add subaccount module

---
+++ examples/create_subaccount.ts
@@ -0,0 +1,24 @@
+import * as BIP39 from "bip39";
+import { CarbonSDK } from "./_sdk";
+import "./_setup";
+
+const TRPC_ENDPOINT = process.env.TRPC_ENDPOINT ?? "http://localhost:26657";
+
+(async () => {
+  const mnemonics = process.env.MNEMONICS ?? BIP39.generateMnemonic();
+  console.log("mnemonics", mnemonics);
+
+  const sdk = await CarbonSDK.instance({
+    network: CarbonSDK.Network.LocalHost,
+    config: {
+      rpcURL: TRPC_ENDPOINT,
+    },
+  });
+  const connectedSDK = await sdk.connectWithMnemonic(mnemonics);
+  console.log("connected sdk");
+
+  const result = await connectedSDK.subaccount.create({
+    subAddress: "tswth1m9h80cfhqn08x7hull0n7pwhlu9k0vp7n92p6y",
+  })
+  console.log(result)
+})().catch(console.error).finally(() => process.exit(0));
