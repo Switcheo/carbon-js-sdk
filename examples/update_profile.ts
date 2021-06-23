7eee0f89f296cd53e6180c1ef6dad9a23a9747cd
xuanlc113 <xuan.lc113@gmail.com>
2021-06-23 10:37:53 +0800

    add profile module

---
+++ examples/update_profile.ts
@@ -0,0 +1,27 @@
+import * as BIP39 from "bip39";
+import { CarbonSDK } from "./_sdk";
+import { BigNumber } from "bignumber.js";
+import Long from "long";
+import "./_setup";
+
+const TRPC_ENDPOINT = process.env.TRPC_ENDPOINT ?? "http://localhost:26657";
+
+(async () => {
+  const mnemonics = process.env.MNEMONICS ?? BIP39.generateMnemonic();
+  console.log("mnemonics", mnemonics);
+
+  const sdk = await CarbonSDK.instance({
+    network: CarbonSDK.Network.LocalHost,
+    config: {
+      rpcURL: TRPC_ENDPOINT,
+    },
+  });
+  const connectedSDK = await sdk.connectWithMnemonic(mnemonics);
+  console.log("connected sdk");
+
+  const result = await connectedSDK.profile.update({
+    username: "aaa",
+    twitter: "",
+  })
+  console.log(result)
+})().catch(console.error).finally(() => process.exit(0));
---
+++ src/modules/profile.ts
@@ -0,0 +1,28 @@
+import { MsgUpdateProfile } from "@carbon-sdk/codec/profile/tx";
+import { CarbonTx } from "@carbon-sdk/util/tx";
+import BaseModule from "./base";
+
+export class ProfileModule extends BaseModule {
+
+  public async update(params: ProfileModule.UpdateProfileParams) {
+    const wallet = this.getWallet();
+
+    const value = MsgUpdateProfile.fromPartial({
+      creator: wallet.bech32Address,
+      username: params.username,
+      twitter: params.twitter,
+    })
+
+    return await wallet.sendTx({
+      typeUrl: CarbonTx.Types.MsgUpdateProfile,
+      value,
+    });
+  }
+}
+
+export namespace ProfileModule {
+  export interface UpdateProfileParams {
+    username: string
+    twitter?: string
+  }
+};
