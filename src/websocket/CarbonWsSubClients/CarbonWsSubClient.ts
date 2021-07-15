3a4dab7502645f618c70c7f822597e776e159f0d
PC <spclight@gmail.com>
2021-07-15 15:39:03 +0800

    refactor: Delete unnecessary files

--- src/websocket/CarbonWsSubClients/CarbonWsSubClient.ts
+++
@@ -1,9 +0,0 @@
-type Subscribe = (channel: string, onMessage: (e: MessageEvent) => void) => void
-
-export class CarbonWsSubClient {
-  subscribe: Subscribe
-
-  constructor(subscribe: Subscribe) {
-    this.subscribe = subscribe
-  }
-}
