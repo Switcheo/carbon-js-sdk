efcd128b5cf83593bcec3b00e9c174e48f929212
PC <spclight@gmail.com>
2021-08-18 17:43:07 +0800

    feat: Export ws

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export * from "./CarbonSDK";
 export * from "./modules";
 export * as Models from "./codec";
 export * from "./util";
+export * from './websocket';
 export { default as CarbonSDK } from "./CarbonSDK";
