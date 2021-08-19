e8cf45e15bcc7bae13a6ea796bc3744a801497d3
sarah-thong <sarah.thong@switcheo.network>
2021-08-19 21:09:48 +0800

    Add mint function example code

---
+++ examples/mint.ts
@@ -0,0 +1,31 @@
+import * as BIP39 from "bip39";
+import { CarbonSDK } from "./_sdk";
+import { BigNumber } from "bignumber.js";
+import "./_setup";
+
+(async () => {
+  const mnemonics = process.env.MNEMONICS ?? BIP39.generateMnemonic();
+  console.log("mnemonics", mnemonics);
+
+  const sdk = await CarbonSDK.instance({
+    network: CarbonSDK.Network.LocalHost,
+    config: {
+      rpcUrl: process.env.TRPC_ENDPOINT,
+    },
+  });
+  const connectedSDK = await sdk.connectWithMnemonic(mnemonics);
+  console.log("connected sdk");
+
+  const response = await sdk.query.coin.TokenAll({})
+  console.log("tokens: ", response.tokens)
+
+  const indivToken = response.tokens[0]
+
+  const result = await connectedSDK.coin.mintToken({
+    creator: connectedSDK.wallet.bech32Address,
+    denom: indivToken.denom,
+    amount: new BigNumber(100000),
+    to: connectedSDK.wallet.bech32Address,
+  })
+  console.log(result)
+})().catch(console.error).finally(() => process.exit(0));
