28967227252dd717708565f1d64f019c5a9d4263
PC <spclight@gmail.com>
2021-08-25 17:01:27 +0800

    feat: Add redelegateTokens and withdrawAllTokens

--- src/modules/staking.ts
+++ src/modules/staking.ts
@@ -1,6 +1,7 @@
 import { TxTypes } from "@carbon-sdk/codec";
 import { MsgWithdrawDelegatorReward } from "@carbon-sdk/codec/cosmos/distribution/v1beta1/tx";
 import { MsgDelegate, MsgUndelegate } from "@carbon-sdk/codec/cosmos/staking/v1beta1/tx";
+import { MsgBeginRedelegate } from "@cosmjs/stargate/build/codec/cosmos/staking/v1beta1/tx";
 import BigNumber from "bignumber.js";
 import BaseModule from "./base";
 
@@ -40,6 +41,24 @@ export class StakingModule extends BaseModule {
     });
   }
 
+  public async redelegateTokens(params: StakingModule.RedelegateTokensParams) {
+    const wallet = this.getWallet();
+    const value = MsgBeginRedelegate.fromPartial({
+      delegatorAddress: params.delegatorAddress ?? wallet.bech32Address,
+      validatorSrcAddress: params.validatorSrcAddress,
+      validatorDstAddress: params.validatorDstAddress,
+      amount: {
+        amount: params.amount.toString(10),
+        denom: "swth",
+      },
+    });
+
+    return wallet.sendTx({
+      typeUrl: TxTypes.MsgBeginRedelegate,
+      value,
+    });
+  }
+
   public async withdrawRewards(params: StakingModule.WithdrawRewardsParams) {
     const wallet = this.getWallet();
     const value = MsgWithdrawDelegatorReward.fromPartial({
@@ -50,6 +69,20 @@ export class StakingModule extends BaseModule {
       typeUrl: TxTypes.MsgWithdrawDelegatorReward,
       value,
     });
+  }
+
+  public async withdrawAllDelegatorRewards(msg: StakingModule.WithdrawAllDelegatorRewardsParams) {
+    const { validatorAddresses, delegatorAddress } = msg
+    const wallet = this.getWallet();
+    const messages: StakingModule.TxMsg[] =
+      validatorAddresses.map((address: string) => (
+        {
+          typeUrl: TxTypes.MsgWithdrawDelegatorReward,
+          value: { validatorAddress: address, delegatorAddress: delegatorAddress }
+        }
+      ))
+
+    return wallet.sendTxs(messages)
   }
 }
 
@@ -64,8 +97,23 @@ export namespace StakingModule {
     validatorAddress: string,
     amount: BigNumber,
   }
+  export interface RedelegateTokensParams {
+    delegatorAddress: string,
+    validatorSrcAddress: string,
+    validatorDstAddress: string,
+    amount: BigNumber,
+  }
   export interface WithdrawRewardsParams {
     delegatorAddress?: string,
     validatorAddress: string,
+  }
+  export interface WithdrawAllDelegatorRewardsParams {
+    delegatorAddress: string,
+    validatorAddresses: string[],
+  }
+  export interface TxMsgValue { }
+  export interface TxMsg<T extends TxMsgValue = TxMsgValue> {
+    typeUrl: string
+    value: T
   }
 };
