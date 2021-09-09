86a831e0e35246e937f0325d7a7c85808b532a64
sarah-thong <sarah.thong@switcheo.network>
2021-09-09 21:40:16 +0800

    Add more ws models

--- src/websocket/models.ts
+++ src/websocket/models.ts
@@ -95,3 +95,28 @@ export interface ClosedPosition {
   market: string;
   realized_pnl: string;
 }
+
+export interface MarketStat {
+  day_high: string
+  day_low: string
+  day_open: string
+  day_close: string
+  day_volume: string
+  day_quote_volume: string
+  index_price: string
+  mark_price: string
+  last_price: string
+  market: string
+  market_type: string
+}
+
+export interface PriceLevel {
+  price: string // string representation of number
+  quantity: string // string representation of number
+}
+
+export interface Leverage {
+  market: string
+  leverage: string // string representation of number
+  originator?: string
+}
