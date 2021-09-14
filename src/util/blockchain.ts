5f6738519f53a3cb39a9cdb1cd36b0b12a7664f6
PC <spclight@gmail.com>
2021-09-14 19:48:07 +0800

    Add: Metamask provider

--- src/util/blockchain.ts
+++ src/util/blockchain.ts
@@ -4,6 +4,7 @@ export enum Blockchain {
   BinanceSmartChain = 'bsc',
   Zilliqa = 'zil',
   Native = 'native',
+  Btc = 'btc',
 };
 
 export const BRIDGE_IDS = {
@@ -13,6 +14,15 @@ export const BRIDGE_IDS = {
 type ChainIds = {
   [id: string]: number
 }
+
+export const ChainNames = {
+  1: 'MainNet',
+  3: 'Ropsten',
+  56: 'BSC MainNet',
+  97: 'BSC TestNet',
+  110: 'ZIL DevNet',
+  111: 'ZIL TestNet',
+} as const
 
 export const CHAIN_IDS: ChainIds = {
   'native': 0,
@@ -25,4 +35,21 @@ export const CHAIN_IDS: ChainIds = {
 
 export function getChainFromID(id: number) {
   return Object.keys(CHAIN_IDS).find((key) => CHAIN_IDS[key] === id)
+}
+
+export const blockchainForChainId = (chainId?: number) => {
+  switch (chainId) {
+    case 0:
+      return Blockchain.Native
+    case 1:
+      return Blockchain.Btc
+    case 2:
+      return Blockchain.Ethereum
+    case 4:
+      return Blockchain.Neo
+    case 6:
+      return Blockchain.BinanceSmartChain
+    case 9:
+      return Blockchain.Zilliqa
+  }
 }
