d42e205f6f59675dd56ad77952f34cefc63ca077
sarah-thong <sarah.thong@switcheo.network>
2021-09-23 13:13:50 +0800

    Remove default main prefix in getAddressBytes function

--- src/util/address.ts
+++ src/util/address.ts
@@ -214,7 +214,8 @@ export const SWTHAddress: SWTHAddressType = {
   },
 
   getAddressBytes: (bech32Address: string, net: Network): Uint8Array => {
-    const prefix = SWTHAddress.getBech32Prefix(net, "main");
+    // Remove "main" because "main" is not a prefix
+    const prefix = SWTHAddress.getBech32Prefix(net);
     const { prefix: b32Prefix, words } = bech32.decode(bech32Address);
     if (b32Prefix !== prefix) {
       throw new Error("Prefix doesn't match");
