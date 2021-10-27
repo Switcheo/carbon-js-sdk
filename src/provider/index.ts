10a838aa23ec5e58559937964c20573d211da6e9
PC <spclight@gmail.com>
2021-10-27 18:28:22 +0800

    wip: Exports

--- src/provider/index.ts
+++ src/provider/index.ts
@@ -2,3 +2,4 @@ export { default as KeplrAccount, KeplrWindow, Keplr, ChainInfo } from "./keplr"
 export { default as SDKProvider } from "./sdk";
 export { default as CosmosLedger } from "./ledger";
 export { MetaMaskChangeNetworkParam, CallContractArgs, MetaMaskSyncResult, MetaMask } from "./metamask";
+export { EthLedgerAccount, NeoLedgerAccount} from "./account"
