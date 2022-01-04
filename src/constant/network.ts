7b40581306740be64947b5a408d2b7959e6a5589
Lee Yik Jiun <yikjiun.lee@switcheo.network>
2022-01-04 13:58:49 +0800

    Update payer urls

--- src/constant/network.ts
+++ src/constant/network.ts
@@ -83,7 +83,7 @@ export const NetworkConfigs: {
     eth: {
       rpcURL: 'https://eth-mainnet.alchemyapi.io/v2/RWHcfoaBKzRpXnLONcEDnVqtUp7StNYl',
       wsURL: 'wss://mainnet.dagger.matic.network',
-      payerURL: `https://eth-payer.switcheo.org`,
+      payerURL: `https://payer.switcheo.org`,
       lockProxyAddr: '0x9a016ce184a22dbf6c17daa59eb7d3140dbd1c54',
       balanceReader: '0xe5e83cdba612672785d835714af26707f98030c3',
       byteCodeHash: '0xc77e5709a69e94d310a6dfb700801758c4caed0385b25bdf82bbdf954e4dd0c3',
@@ -92,7 +92,7 @@ export const NetworkConfigs: {
     bsc: {
       rpcURL: 'https://bsc-dataseed2.binance.org/',
       wsURL: '',
-      payerURL: `https://bsc-payer.switcheo.org`,
+      payerURL: `https://payer.carbon.network`,
       lockProxyAddr: '0xb5d4f343412dc8efb6ff599d790074d0f1e8d430',
       balanceReader: '0x2b18c5e1edaa7e27d40fec8d0b7d96c5eefa35df',
       byteCodeHash: '0x1b147c1cef546fcbcc1284df778073d65b90f80d5b649a69d5f8a01e186c0ec1',
@@ -132,7 +132,7 @@ export const NetworkConfigs: {
     eth: {
       rpcURL: 'https://eth-ropsten.alchemyapi.io/v2/2KD9F3mFPNMfflSqZsPuTKmK_w7fFfut',
       wsURL: 'wss://ropsten.dagger.matic.network',
-      payerURL: `http://13.251.218.38:7001`,
+      payerURL: `https://test-payer.carbon.network`,
       lockProxyAddr: '0x91f453851e297524749a740d53cf54a89231487c',
       balanceReader: '0xa74c81866c5bfff6684aa8edf35a5de8c3b9f173',
       byteCodeHash: '0xc77e5709a69e94d310a6dfb700801758c4caed0385b25bdf82bbdf954e4dd0c3',
@@ -141,7 +141,7 @@ export const NetworkConfigs: {
     bsc: {
       rpcURL: 'https://data-seed-prebsc-2-s3.binance.org:8545/',
       wsURL: '',
-      payerURL: `http://54.255.42.175:8001`,
+      payerURL: `https://test-payer.carbon.network`,
       lockProxyAddr: '0x7c2b13d656d222cb79670e301dd826dc5b8dc20c',
       balanceReader: '0x25c22f65cb820e787a13951f295d0b86db7b07b5',
       byteCodeHash: '0x1b147c1cef546fcbcc1284df778073d65b90f80d5b649a69d5f8a01e186c0ec1',
@@ -181,7 +181,7 @@ export const NetworkConfigs: {
     eth: {
       rpcURL: 'https://eth-ropsten.alchemyapi.io/v2/2KD9F3mFPNMfflSqZsPuTKmK_w7fFfut',
       wsURL: 'wss://ropsten.dagger.matic.network',
-      payerURL: `http://13.251.218.38:7001`,
+      payerURL: `https://dev-payer.carbon.network`,
       lockProxyAddr: '0x91f453851e297524749a740d53cf54a89231487c',
       balanceReader: '0xa74c81866c5bfff6684aa8edf35a5de8c3b9f173',
       byteCodeHash: '0xc77e5709a69e94d310a6dfb700801758c4caed0385b25bdf82bbdf954e4dd0c3',
@@ -190,7 +190,7 @@ export const NetworkConfigs: {
     bsc: {
       rpcURL: 'https://data-seed-prebsc-2-s3.binance.org:8545/',
       wsURL: '',
-      payerURL: `http://13.251.218.38:8001`,
+      payerURL: `https://dev-payer.carbon.network`,
       lockProxyAddr: '0x7c2b13d656d222cb79670e301dd826dc5b8dc20c',
       balanceReader: '0x25c22f65cb820e787a13951f295d0b86db7b07b5',
       byteCodeHash: '0x1b147c1cef546fcbcc1284df778073d65b90f80d5b649a69d5f8a01e186c0ec1',
@@ -230,7 +230,7 @@ export const NetworkConfigs: {
     eth: {
       rpcURL: 'https://ropsten.infura.io/v3/e4dd457b33124bbda7e17500e6efbc27',
       wsURL: 'wss://ropsten.dagger.matic.network',
-      payerURL: `http://localhost:7001`,
+      payerURL: `http://localhost:8001`,
       lockProxyAddr: '',
       balanceReader: '',
       byteCodeHash: '',
