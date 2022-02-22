cb12b84a22a67c1a2070137c4b495a1dcbe190f0
Thong Yuan Yu Sarah <sarah.thong@switcheo.network>
2022-02-22 10:28:46 +0800

    Complete export of O3Wallet and O3Types

--- src/provider/index.ts
+++ src/provider/index.ts
@@ -3,5 +3,6 @@ export { AminoTypesMap } from "./amino";
 export { ChainInfo, default as KeplrAccount, Keplr, KeplrWindow } from "./keplr";
 export { default as CosmosLedger } from "./ledger";
 export { CallContractArgs, EVMChain, MetaMask, MetaMaskChangeNetworkParam, MetaMaskSyncResult } from "./metamask";
+export { O3Types, O3Wallet } from "./o3";
 export { default as SDKProvider } from "./sdk";
 export { Zilpay, ZilPayChangeNetworkParam } from "./zilpay";
