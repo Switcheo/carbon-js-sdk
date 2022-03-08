81ceb07851a136e9af47bade48a122aca77a8ba7
brandonwong91 <brandonwongck@gmail.com>
2022-03-08 13:22:11 +0800

    Add decoding for ibc tendermint header

--- src/util/tx.ts
+++ src/util/tx.ts
@@ -5,6 +5,8 @@ import { SignerData } from "@cosmjs/stargate";
 import { registry } from "@carbon-sdk/codec";
 import * as CosmosModels from "@carbon-sdk/codec/cosmos-models";
 import { SWTHAddress, SWTHAddressOptions } from "./address";
+import { GenericUtils } from "@carbon-sdk/util";
+import Long from "long";
 
 export interface TxBody extends Omit<CosmosModels.Tx.TxBody, "messages"> {
   messages: unknown[]
@@ -12,6 +14,24 @@ export interface TxBody extends Omit<CosmosModels.Tx.TxBody, "messages"> {
 export interface Tx extends Omit<CosmosModels.Tx.Tx, "body"> {
   body?: TxBody
 }
+
+const decodeNestedMsg = (obj: any) => {
+  for (const key in obj) {
+
+    if (typeof obj[key] === "object" && obj[key].low !== undefined && obj[key].high !== undefined && obj[key].unsigned !== undefined) {
+      obj[key] = Long.fromValue(obj[key]).toString()
+    } else if (obj[key] instanceof Uint8Array) {
+      obj[key] = GenericUtils.toTxHash(obj[key])
+    } else if (typeof obj[key] === "object" && obj[key].typeUrl !== undefined && obj[key].value !== undefined && Object.values(TxTypes).includes(obj[key].typeUrl)) {
+      obj[key].value = registry.decode(obj[key])
+      obj[key] = decodeNestedMsg(obj[key])
+    } else if (obj[key] instanceof Object) {
+      obj[key] = decodeNestedMsg(obj[key]);
+    }
+
+  }
+  return obj;
+};
 
 export const decode = (bytes?: Uint8Array | Buffer): Tx | undefined => {
   if (!bytes) return bytes;
@@ -25,8 +45,8 @@ export const decode = (bytes?: Uint8Array | Buffer): Tx | undefined => {
       // override original UInt8Array messages with decoded messages
       messages: decodedTx.body.messages.map(message => ({
         typeUrl: message.typeUrl,
-        value: registry.decode(message),
-      })),
+        value: decodeNestedMsg(registry.decode(message)),
+    })),
     };
   } else {
     delete carbonTx.body;
