d526f2163c05d2abf65e03f2c8de31b366cdc027
Steven Khong <khongwh@gmail.com>
2022-03-18 16:02:20 +0800

    Fix default gas fees

--- src/constant/generic.ts
+++ src/constant/generic.ts
@@ -4,10 +4,10 @@ import BigNumber from "bignumber.js";
 
 // 0.0000001 SWTH
 // 10 SWTH sats
-export const DEFAULT_GAS_PRICE = new BigNumber("769.23077");
-export const DEFAULT_GAS = new BigNumber("130000");
+export const DEFAULT_GAS_PRICE = new BigNumber(10);
+export const DEFAULT_GAS = new BigNumber(10000000);
 
 export const DEFAULT_FEE: StdFee = {
-  amount: coins(DEFAULT_GAS_PRICE.times(DEFAULT_GAS).dp(0).toNumber(), "swth"),
+  amount: coins(DEFAULT_GAS_PRICE.times(DEFAULT_GAS).dp(0).toString(), "swth"),
   gas: DEFAULT_GAS.toString(10),
 };
