bef9eafa7308d68c1fe7abda9813241fe75063f9
Thong Yuan Yu Sarah <65583522+sarah-thong@users.noreply.github.com>
2022-03-21 16:20:28 +0800

    Temp removal of keplr dependency to fix Safari error (#133)
    * Remove @keplr-wallet/stores dependency
    * Remove instances that use @keplr-wallet/stores

--- src/provider/index.ts
+++ src/provider/index.ts
@@ -1,6 +1,6 @@
 export { EthLedgerAccount, EthLedgerSigner, NeoLedgerAccount } from "./account";
 export { AminoTypesMap } from "./amino";
-export { ChainInfo, CoinPretty, default as KeplrAccount, Keplr, KeplrWindow, RootStore, createRootStore } from "./keplr";
+export { ChainInfo, default as KeplrAccount, Keplr, KeplrWindow, RootStore, createRootStore } from "./keplr";
 export { default as CosmosLedger } from "./ledger";
 export { CallContractArgs, EVMChain, MetaMask, MetaMaskChangeNetworkParam, MetaMaskSyncResult } from "./metamask";
 export { O3Types, O3Wallet } from "./o3";
