8c4bc838067d4a6a0c7e7cf553e5236ab6af0c90
sarah-thong <sarah.thong@switcheo.network>
2022-03-21 15:50:49 +0800

    Remove instances that use @keplr-wallet/stores

--- src/provider/keplr/index.ts
+++ src/provider/keplr/index.ts
@@ -1,5 +1,5 @@
 export { default } from "./KeplrAccount";
 export { KeplrWindow } from "./window";
 export { Keplr, ChainInfo } from "@keplr-wallet/types";
-export { CoinPretty } from "@keplr-wallet/unit";
+// export { CoinPretty } from "@keplr-wallet/unit";
 export { RootStore, createRootStore } from "./KeplrStore";
