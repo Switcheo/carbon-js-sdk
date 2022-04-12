da5e725c319110f72123da99622fa349240ab08e
sarah-thong <sarah.thong@switcheo.network>
2022-04-12 11:17:01 +0800

    Add blockchain map for ibc tokens

--- src/util/ibc.ts
+++ src/util/ibc.ts
@@ -1,6 +1,7 @@
 import { ChainInfoExplorerTmRpc, EmbedChainInfosInit, ibcWhitelist, totalAssetObj } from "@carbon-sdk/constant";
 import { KeplrAccount } from "@carbon-sdk/provider";
 import { Hash } from "@keplr-wallet/crypto";
+import { AppCurrency } from "@keplr-wallet/types";
 import { SimpleMap } from "./type";
 
 // Create IBC minimal denom
@@ -23,4 +24,20 @@ export const EmbedChainInfos = Object.values(EmbedChainInfosInit).reduce((prev: 
   }
 	prev[chainInfo.chainId] = chainInfo;
 	return prev;
+}, {});
+
+export const BlockchainMap = Object.values(EmbedChainInfos).reduce((prev: SimpleMap<string>, chainInfo: ChainInfoExplorerTmRpc) => {
+	if (!ibcWhitelist.includes(chainInfo.chainId)) {
+		return prev;
+	}
+	const newPrev = prev;
+	chainInfo.currencies.forEach((currency: AppCurrency) => {
+		if (currency.coinDenom.toLowerCase() === "swth") {
+			newPrev[currency.coinMinimalDenom] = chainInfo.chainId;
+		} else {
+			const ibcAddr = makeIBCMinimalDenom("channel-0", currency.coinMinimalDenom);
+			newPrev[ibcAddr] = chainInfo.chainId;
+		}
+	});
+	return newPrev;
 }, {});
