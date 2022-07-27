a6f09cc2f41797f88f5c7bfed7b61bc46b34f084
Steven Khong <khongwh@gmail.com>
2022-07-27 12:07:46 +0800

    Fix init error for new accounts

--- src/wallet/CarbonWallet.ts
+++ src/wallet/CarbonWallet.ts
@@ -508,21 +508,26 @@ export class CarbonWallet {
   public async reloadAccountSequence() {
     if (this.sequenceInvalidated) this.sequenceInvalidated = false;
 
-    const info = await this.getQueryClient().chain.getSequence(this.bech32Address);
-
-    const pubkey = this.accountInfo?.pubkey ?? {
-      type: "tendermint/PubKeySecp256k1",
-      value: this.publicKey.toString("base64"),
-    };
-
-    const chainId = this.accountInfo?.chainId ?? this.chainId ?? (await this.getQueryClient().chain.getChainId());
-
-    this.accountInfo = {
-      ...info,
-      address: this.bech32Address,
-      pubkey,
-      chainId,
-    };
+    try {
+      const info = await this.getQueryClient().chain.getSequence(this.bech32Address);
+  
+      const pubkey = this.accountInfo?.pubkey ?? {
+        type: "tendermint/PubKeySecp256k1",
+        value: this.publicKey.toString("base64"),
+      };
+  
+      const chainId = this.accountInfo?.chainId ?? this.chainId ?? await this.getQueryClient().chain.getChainId();
+  
+      this.accountInfo = {
+        ...info,
+        address: this.bech32Address,
+        pubkey,
+        chainId,
+      };
+    } catch (error: any) {
+      if (!this.isNewAccountError(error))
+        throw error;
+    }
   }
 
   private estimateTxFee(messages: readonly EncodeObject[]) {
@@ -542,6 +547,10 @@ export class CarbonWallet {
       ],
       gas: DEFAULT_GAS.times(messages.length).toString(10),
     };
+  }
+
+  private isNewAccountError = (error?: Error) => {
+    return error?.message?.includes("Account does not exist on chain. Send some tokens there before trying to query sequence.");
   }
 
   private isNonceMismatchError = (error?: Error) => {
