733d75e4925b04cab1a0bd59fcba1076e4afc68b
sarah-thong <sarah.thong@switcheo.network>
2022-09-20 10:57:49 +0800

    Modify Order History struct

--- src/websocket/models.ts
+++ src/websocket/models.ts
@@ -27,11 +27,10 @@ export interface HistoryOrder {
   filled: string;
   id: string;
   initiator: string;
-  is_liquidation: boolean;
-  is_post_only: boolean;
-  is_reduce_only: boolean;
+  is_liquidation?: boolean;
+  is_post_only?: boolean;
+  is_reduce_only?: boolean;
   market: string;
-  order_id: string;
   order_type: string;
   price: string;
   quantity: string;
