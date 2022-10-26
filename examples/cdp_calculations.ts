4a8e976f3ae69fd1c4cac0bc9bb8320b02c06baf
Russell Chan <russellcxl@gmail.com>
2022-10-26 16:41:17 +0800

    add getModuleTotalDebtUsdVal

--- examples/cdp_calculations.ts
+++ examples/cdp_calculations.ts
@@ -16,51 +16,54 @@ import "./_setup";
 
   const account = "tswth13zw3p5902nu9p0nq7gvsvjcyruz9hdszgxqw4s"
 
-  // const connectedSDK = await sdk.connectWithMnemonic(mnemonics);
-  // console.log("connected sdk");
-  //
-  // const accData = await sdk.cdp.getAccountData("tswth13zw3p5902nu9p0nq7gvsvjcyruz9hdszgxqw4s")
-  // console.log("\ngetAccountData", JSON.stringify(accData));
-  //
-  // const debt = await sdk.query.cdp.AccountDebt({ account: "tswth17ktnyhxuxe4s9a2u55gnt5qyalhksws7thnswq", denom: "eth"})
-  // console.log("\nAccountDebt", JSON.stringify(debt))
-  //
-  // const tokenDebts = await sdk.query.cdp.TokenDebtsAll({})
-  // console.log("\nTokenDebtsAll", JSON.stringify(tokenDebts))
-  //
-  // const tokenDebt = await sdk.cdp.getTotalAccountTokenDebt("tswth188xswz2hyu3shtypxfaz7zz97zuevk9k2cxtp9", "eth")
-  // console.log("\ngetTotalAccountTokenDebt", JSON.stringify(tokenDebt))
-  //
-  // const tokenDebtUSD = await sdk.cdp.getTotalAccountTokenDebtUsdVal("tswth188xswz2hyu3shtypxfaz7zz97zuevk9k2cxtp9", "eth")
-  // console.log("\ngetTotalAccountTokenDebtUsdVal", JSON.stringify(tokenDebtUSD))
-  //
-  // const cim = await sdk.cdp.recalculateCIM("eth")
-  // console.log("\nrecalculateCIM", cim.toNumber())
-  //
-  // const stablecoinDebt = await sdk.query.cdp.StablecoinDebt({})
-  // console.log("\nStablecoinDebt", JSON.stringify(stablecoinDebt))
-  //
-  // const collateralsAll = await sdk.query.cdp.CollateralsAll({})
-  // console.log("\nCollateralsAll", JSON.stringify(collateralsAll))
-  //
-  // const accountStablecoin = await sdk.query.cdp.AccountStablecoin({account: account})
-  // console.log("\nAccountStablecoin", JSON.stringify(accountStablecoin))
-  //
-  // const accountCollateral = await sdk.query.cdp.AccountCollateral({account: "tswth188xswz2hyu3shtypxfaz7zz97zuevk9k2cxtp9", cdpDenom: "cdp/eth"})
-  // console.log("\nAccountCollateral", accountCollateral)
-  //
-  // const accountCollaterals = await sdk.query.cdp.AccountCollaterals({account: "tswth188xswz2hyu3shtypxfaz7zz97zuevk9k2cxtp9"})
-  // console.log("\nAccountCollaterals", accountCollaterals)
-  //
-  // const ratio = await sdk.cdp.getCdpToActualRatio("cdp/eth");
-  // console.log("\ngetCdpToActualRatio", JSON.stringify(ratio))
-  //
-  // const amt = new BigNumber("1000000")
-  // const cdpTokenValue = await sdk.cdp.getCdpTokenUsdVal("cdp/usdc", amt);
-  // console.log("\ngetCdpTokenUsdVal", cdpTokenValue?.toNumber())
+  const connectedSDK = await sdk.connectWithMnemonic(mnemonics);
+  console.log("connected sdk");
+
+  const accData = await sdk.cdp.getAccountData("tswth13zw3p5902nu9p0nq7gvsvjcyruz9hdszgxqw4s")
+  console.log("\ngetAccountData", JSON.stringify(accData));
+
+  const debt = await sdk.query.cdp.AccountDebt({ account: "tswth17ktnyhxuxe4s9a2u55gnt5qyalhksws7thnswq", denom: "eth"})
+  console.log("\nAccountDebt", JSON.stringify(debt))
+
+  const tokenDebts = await sdk.query.cdp.TokenDebtsAll({})
+  console.log("\nTokenDebtsAll", JSON.stringify(tokenDebts))
+
+  const tokenDebt = await sdk.cdp.getTotalAccountTokenDebt("tswth188xswz2hyu3shtypxfaz7zz97zuevk9k2cxtp9", "eth")
+  console.log("\ngetTotalAccountTokenDebt", JSON.stringify(tokenDebt))
+
+  const tokenDebtUSD = await sdk.cdp.getTotalAccountTokenDebtUsdVal("tswth188xswz2hyu3shtypxfaz7zz97zuevk9k2cxtp9", "eth")
+  console.log("\ngetTotalAccountTokenDebtUsdVal", JSON.stringify(tokenDebtUSD))
+
+  const cim = await sdk.cdp.recalculateCIM("eth")
+  console.log("\nrecalculateCIM", cim.toNumber())
+
+  const stablecoinDebt = await sdk.query.cdp.StablecoinDebt({})
+  console.log("\nStablecoinDebt", JSON.stringify(stablecoinDebt))
+
+  const collateralsAll = await sdk.query.cdp.CollateralsAll({})
+  console.log("\nCollateralsAll", JSON.stringify(collateralsAll))
+
+  const accountStablecoin = await sdk.query.cdp.AccountStablecoin({account: account})
+  console.log("\nAccountStablecoin", JSON.stringify(accountStablecoin))
+
+  const accountCollateral = await sdk.query.cdp.AccountCollateral({account: "tswth188xswz2hyu3shtypxfaz7zz97zuevk9k2cxtp9", cdpDenom: "cdp/eth"})
+  console.log("\nAccountCollateral", accountCollateral)
+
+  const accountCollaterals = await sdk.query.cdp.AccountCollaterals({account: "tswth188xswz2hyu3shtypxfaz7zz97zuevk9k2cxtp9"})
+  console.log("\nAccountCollaterals", accountCollaterals)
+
+  const ratio = await sdk.cdp.getCdpToActualRatio("cdp/eth");
+  console.log("\ngetCdpToActualRatio", JSON.stringify(ratio))
+
+  const amt = new BigNumber("1000000")
+  const cdpTokenValue = await sdk.cdp.getCdpTokenUsdVal("cdp/usdc", amt);
+  console.log("\ngetCdpTokenUsdVal", cdpTokenValue?.toNumber())
 
   const totalCollateral = await sdk.cdp.getModuleTotalCollateralUsdVal();
   console.log("\ngetModuleTotalCollateralUsdVal", totalCollateral?.toNumber())
+
+  const totalDebt = await sdk.cdp.getModuleTotalDebtUsdVal();
+  console.log("\ngetModuleTotalDebtUsdVal", totalDebt?.toNumber())
 
 
 })().catch(console.error).finally(() => process.exit(0));
