f5e8daffa254a392152c40cf47352439f070e6c6
Steven Khong <khongwh@gmail.com>
2022-11-28 22:38:01 +0800

    Revert "fix getTotalTokenDebt to not use accrued interest"
    This reverts commit 98041ccf74269624cde300c03b59786483eee5f7.

--- src/modules/cdp.ts
+++ src/modules/cdp.ts
@@ -558,12 +558,12 @@ export class CDPModule extends BaseModule {
     if (!debtInfo)
       throw new Error("unable to retrieve debt info");
     const principal = bnOrZero(debtInfo.totalPrincipal)
-    const interest = principal.multipliedBy(bnOrZero(debtInfo.cumulativeInterestMultiplier).shiftedBy(-18).minus(1))
+    const accumInterest = bnOrZero(debtInfo.totalAccumulatedInterest)
     const cdpParamsRsp = await this.sdkProvider.query.cdp.Params(QueryParamsRequest.fromPartial({}))
     const interestFee = bnOrZero(cdpParamsRsp.params?.interestFee)
-    const interestWithoutFee = interest.times(BN_10000.minus(interestFee)).dividedToIntegerBy(BN_10000);
+    const interest = accumInterest.times(BN_10000.minus(interestFee)).dividedToIntegerBy(BN_10000);
 
-    return principal.plus(interestWithoutFee);
+    return principal.plus(interest);
   }
 
   public async getTotalAccountTokenDebt(account: string, denom: string, debt?: Debt, debtInfo?: DebtInfo) {
