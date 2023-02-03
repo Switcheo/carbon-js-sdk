ca3d2c501f89851185a13931f05b548993029a43
Russell Chan <russellcxl@gmail.com>
2023-02-03 16:04:07 +0800

    add createToken and addBridgeAddr txs

--- src/modules/coin.ts
+++ src/modules/coin.ts
@@ -1,7 +1,8 @@
-import { MsgMintToken, MsgWithdraw } from "@carbon-sdk/codec/coin/tx";
+import { MsgMintToken, MsgWithdraw, MsgAddBridgeAddress, MsgCreateToken } from "@carbon-sdk/codec/coin/tx";
 import { CarbonTx } from "@carbon-sdk/util";
 import BigNumber from "bignumber.js";
 import BaseModule from "./base";
+import Long from "long";
 
 export class CoinModule extends BaseModule {
   public async createWithdrawal(params: CoinModule.CreateWithdrawalParams, opts?: CarbonTx.SignTxOpts) {
@@ -43,6 +44,48 @@ export class CoinModule extends BaseModule {
       opts
     );
   }
+
+  public async addBridgeAddress(params: CoinModule.AddBridgeAddressParams, opts?: CarbonTx.SignTxOpts) {
+    const wallet = this.getWallet();
+    const value = MsgAddBridgeAddress.fromPartial({
+      creator: params.creator ?? wallet.bech32Address,
+      chainId: params.chainId,
+      bridgeId: params.bridgeId,
+      bridgeAddress: params.bridgeAddress,
+    })
+    return await wallet.sendTx(
+        {
+          typeUrl: CarbonTx.Types.MsgAddBridgeAddress,
+          value,
+        },
+        opts
+    );
+  }
+
+  public async createToken(params: CoinModule.CreateTokenParams, opts?: CarbonTx.SignTxOpts) {
+    const wallet = this.getWallet();
+    const value = MsgCreateToken.fromPartial({
+      creator: params.creator ?? wallet.bech32Address,
+      createTokenParams: {
+        creator: params.creator ?? wallet.bech32Address,
+        name: params.name,
+        symbol: params.symbol,
+        decimals: params.decimals,
+        chainId: params.chainId,
+        bridgeId: params.bridgeId,
+        bridgeAddress: params.bridgeAddress,
+        tokenAddress: params.tokenAddress,
+      }
+    })
+    return await wallet.sendTx(
+        {
+          typeUrl: CarbonTx.Types.MsgCreateToken,
+          value,
+        },
+        opts
+    );
+  }
+
 }
 
 export namespace CoinModule {
@@ -59,5 +102,23 @@ export namespace CoinModule {
     denom: string;
     amount: BigNumber;
     to?: string;
+  }
+
+  export interface AddBridgeAddressParams {
+    creator?: string;
+    chainId: Long;
+    bridgeId: Long;
+    bridgeAddress: string;
+  }
+
+  export interface CreateTokenParams {
+    creator?: string;
+    name: string;
+    symbol: string;
+    decimals: Long;
+    chainId: Long;
+    bridgeId: Long;
+    bridgeAddress: string;
+    tokenAddress: string;
   }
 }
