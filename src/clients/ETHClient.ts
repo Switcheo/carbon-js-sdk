fef77fbea67b5a4b2b2b8e8c678d68000b34dbbd
Jayden Teoh <90945854+JaydenTeoh@users.noreply.github.com>
2023-02-03 13:13:14 +0800

    fix testnet zilbridge errors (#248)
    * fix testnet zilbridge errors
    
    * make changes according to comments
    
    * make further amendments according to review
    
    * change testnet chain id 1
    
    ---------
    
    Co-authored-by: sarah-thong <sarah.thong@switcheo.network>

--- src/clients/ETHClient.ts
+++ src/clients/ETHClient.ts
@@ -158,15 +158,18 @@ export class ETHClient {
     const networkConfig = this.getNetworkConfig();
     const rpcProvider = this.getProvider();
 
-    if (!recoveryAddress.match(/^swth[a-z0-9]{39}$/)) {
+    const recoveryAddrRegex = new RegExp(`^${networkConfig.Bech32Prefix}[a-z0-9]{39}$`)
+    if (!recoveryAddress.match(recoveryAddrRegex)) {
       throw new Error("Invalid recovery address");
     }
+
+    const carbonNetwork = networkConfig.network;
 
     const fromTokenId = fromToken.id;
     const fromTokenAddress = appendHexPrefix(fromToken.tokenAddress);
     const toTokenDenom = toToken.denom;
 
-    const recoveryAddressHex = ethers.utils.hexlify(AddressUtils.SWTHAddress.getAddressBytes(recoveryAddress, CarbonSDK.Network.MainNet));
+    const recoveryAddressHex = ethers.utils.hexlify(AddressUtils.SWTHAddress.getAddressBytes(recoveryAddress, carbonNetwork));
 
     const fromAssetHash = ethers.utils.hexlify(ethers.utils.toUtf8Bytes(fromTokenId));
     const toAssetHash = ethers.utils.hexlify(ethers.utils.toUtf8Bytes(toTokenDenom));
@@ -177,7 +180,7 @@ export class ETHClient {
 
     const tokenCreator = fromToken.creator;
 
-    const targetAddressBytes = AddressUtils.SWTHAddress.getAddressBytes(tokenCreator, CarbonSDK.Network.MainNet);
+    const targetAddressBytes = AddressUtils.SWTHAddress.getAddressBytes(tokenCreator, carbonNetwork);
     const targetProxyHash = ethers.utils.hexlify(targetAddressBytes);
 
     const ethAmount = fromToken.tokenAddress === NativeTokenHash ? amount : BN_ZERO;
