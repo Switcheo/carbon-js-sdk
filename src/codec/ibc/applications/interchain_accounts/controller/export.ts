7f087381bb302efb12042f294396288bad3ba53f
sarah-thong <sarah.thong@switcheo.network>
2023-03-06 08:54:24 +0800

    Update codecs

--- src/codec/ibc/applications/interchain_accounts/controller/export.ts
+++ src/codec/ibc/applications/interchain_accounts/controller/export.ts
@@ -1,2 +1,2 @@
 export { Params } from "./v1/controller";
-export { QueryParamsRequest, QueryParamsResponse } from "./v1/query";
+export { QueryInterchainAccountRequest, QueryInterchainAccountResponse, QueryParamsRequest, QueryParamsResponse } from "./v1/query";
