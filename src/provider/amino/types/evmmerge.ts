596ad18f92e0ac5700205c3f8e836d6b4805a33a
Randy <randy75828@gmail.com>
2023-04-12 18:53:50 +0800

    add amino for evm, feemarket and evmmerge modules
    add amino for evm

---
+++ src/provider/amino/types/evmmerge.ts
@@ -0,0 +1,20 @@
+import { TypeUtils } from "@carbon-sdk/util";
+import * as CarbonTx from "@carbon-sdk/util/tx";
+import { AminoConverter } from "@cosmjs/stargate";
+import { AminoInit, generateAminoType } from "../utils";
+
+const TxTypes: TypeUtils.SimpleMap<string> = {
+  MergeAccount: "evmmerge/MsgMergeAccount",
+};
+
+const MsgMergeAccount: AminoInit = {
+  aminoType: TxTypes.MergeAccount,
+  valueMap: {},
+};
+
+
+const EvmMergeAmino: TypeUtils.SimpleMap<AminoConverter> = {
+  [CarbonTx.Types.MsgMergeAccount]: generateAminoType(MsgMergeAccount),
+};
+
+export default EvmMergeAmino;
