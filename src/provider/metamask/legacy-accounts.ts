2b314ac6399bebc49eb047b2026ecb9a2e86fb58
Randy <randy75828@gmail.com>
2023-05-16 14:26:41 +0800

    update legacy accounts test

--- src/provider/metamask/legacy-accounts.ts
+++ src/provider/metamask/legacy-accounts.ts
@@ -1,3 +1,4 @@
+// As of 16 May 2023
 export const LEGACY_ACCOUNTS_TESTNET = {
     "Ethereum": [
         "0x32c1069dcf38a7294ee9633fcb897c18cabd1668",
@@ -10,10 +11,20 @@ export const LEGACY_ACCOUNTS_TESTNET = {
         "0x7d831dd3a6f1553b9b995f499f4d31d4935b34f8",
         "0xb88cb290efff1017a27db762fba8f15e754fdd67",
         "0xc120adde16e45c1a36e65fb9bdb5bd8b1f620c19",
-        "0x0897fbc3eb27992cadbffa506e1403d180c94735"
+        "0x0897fbc3eb27992cadbffa506e1403d180c94735",
+        "0x49367893FF5e5481312159D1EC7dfFd1bCD082B7",
+        "0x6fC1b1665BF2EC94d3d936EAb45ACa6b58eE2797",
+        "0x3619d35d2ca763d63387138Fff3a13A5fFE7c8Ce",
+        "0xEEe3c236312a81f3CC3CB78763968e24355b513F",
+        "0x184268C2eA4332234d6Ee5b0bA1C212E63fa55Cc",
+        "0x05a2F90551Dcba19C42c6E85FB00843C0859a427",
+        "0xad045ec15DbBe6FebE8e7D1EC85Eb7D496c4ebe1",
+        "0xcFa83a56f1479BB9CD5a25f36a0C3E44EE08D7DB",
+        "0x8e2C2E908195F87Bc6C14fAEC31F7726d094b3c6"
     ]
 }
 
+// To be updated
 export const LEGACY_ACCOUNTS_MAINNET = {
     "Ethereum": [
         "0x32c1069dcf38a7294ee9633fcb897c18cabd1668",
