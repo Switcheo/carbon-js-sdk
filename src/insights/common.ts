90ccd1788ffaa507151f6d0e2c74f2b432ebacc9
Lim Jun Hong, Don <don.limjunhong@gmail.com>
2023-07-21 11:00:46 +0800

    Add positions stats params

--- src/insights/common.ts
+++ src/insights/common.ts
@@ -36,7 +36,7 @@ export const InsightsEndpoints = {
   "position/view": "/position/view/:view",
   "position/liquidation": "/position/liquidation",
   "position/liquidation/engine": "/position/liquidation/engine",
-  "position/stats": "/position/stats",
+  "position/stats": "/position/stats/:address",
 
   // Competition api
   "competition/list": "/competition/list",
