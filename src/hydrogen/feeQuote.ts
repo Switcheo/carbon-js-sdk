85740f20f8835fd173125ac0fd9a282d93585bb8
yan-soon <soonxiangyan@gmail.com>
2023-07-31 16:06:26 +0800

    add optional other_token_fees field to getFeeQuote response, refactor duplicate feeQuote type

--- src/hydrogen/feeQuote.ts
+++ src/hydrogen/feeQuote.ts
@@ -17,15 +17,12 @@ export interface GetFeeQuoteResponse {
   withdrawal_fee: string;
   created_at: dayjs.Dayjs;
   expires_at: dayjs.Dayjs;
+  other_token_fees?: FeeDenomResponse[] | undefined
 }
 
-export interface FeeQuote {
-  id: number;
-  token_denom: string;
-  blockchain: string;
-  create_wallet_fee: string;
-  deposit_fee: string;
-  withdrawal_fee: string;
-  created_at: dayjs.Dayjs;
-  expires_at: dayjs.Dayjs;
+interface FeeDenomResponse {
+  denom: string,
+  withdrawal_fee: string,
+  deposit_fee: string,
+  create_wallet_fee: string
 }
