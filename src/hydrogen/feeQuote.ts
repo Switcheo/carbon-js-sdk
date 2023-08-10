605e78896a6b174c86b31b36737d05fbcd0693eb
Thong Yuan Yu Sarah <65583522+sarah-thong@users.noreply.github.com>
2023-08-10 12:59:02 +0800

    Cherry pick/update hydrogen fee quote (#371)
    * update feeQuote api to include fee_denoms as an optional body
    
    * add optional other_token_fees field to getFeeQuote response, refactor duplicate feeQuote type
    
    * extend feeRequestQuery and remove unnecessary undefined type
    
    * update fetch request to support post
    
    * remove unused imports
    
    ---------
    
    Co-authored-by: yan-soon <soonxiangyan@gmail.com>

--- src/hydrogen/feeQuote.ts
+++ src/hydrogen/feeQuote.ts
@@ -2,6 +2,7 @@ import dayjs from "dayjs";
 
 export interface GetFeeQuoteRequest {
   token_denom: string;
+  fee_denoms?: string[]
 }
 
 export interface GetFeeQuoteResponse {
@@ -13,15 +14,12 @@ export interface GetFeeQuoteResponse {
   withdrawal_fee: string;
   created_at: dayjs.Dayjs;
   expires_at: dayjs.Dayjs;
+  other_token_fees?: FeeDenomResponse[]
 }
 
-export interface FeeQuote {
-  id: number;
-  token_denom: string;
-  blockchain: string;
-  create_wallet_fee: string;
-  deposit_fee: string;
-  withdrawal_fee: string;
-  created_at: dayjs.Dayjs;
-  expires_at: dayjs.Dayjs;
+interface FeeDenomResponse {
+  denom: string,
+  withdrawal_fee: string,
+  deposit_fee: string,
+  create_wallet_fee: string
 }
