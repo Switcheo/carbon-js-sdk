aa42cc98afd97328e0aff6d7460f815888f485b6
Randy <randy75828@gmail.com>
2023-09-21 00:05:16 +0800

    update checkWalletSignatureCompatibility error message

--- src/wallet/CarbonWallet.ts
+++ src/wallet/CarbonWallet.ts
@@ -394,7 +394,7 @@ export class CarbonWallet {
     const isEvmWallet = this.isEvmWallet()
     if (hasEvmAddressBalances && !hasCarbonBalances && !isEvmWallet) {
       this.sequenceInvalidated = true
-      throw new Error('Request rejected.')
+      throw new Error('Transaction is not allowed from a non-evm wallet for an account with only funds in evm address')
     }
   }
 
