14d2dcbe7a80211fc29fc62f10fffde6beb63d43
Thong Yuan Yu Sarah <sarah.thong@switcheo.network>
2023-12-22 10:50:18 +0800

    Remove redundant quotation marks

--- scripts/config.ts
+++ scripts/config.ts
@@ -13,24 +13,24 @@ export const whitelistCosmosExports: { [name: string]: string } = {
 };
 
 export const whitelistEthermintExports: { [name: string]: string } = {
-  'Crypto': 'ethermint/crypto/v1/ethsecp256k1',
-  'Evm': 'ethermint/evm/v1',
-  'Feemarket': 'ethermint/feemarket/v1',
-  'Types': 'ethermint/types/v1',
+  Crypto: 'ethermint/crypto/v1/ethsecp256k1',
+  Evm: 'ethermint/evm/v1',
+  Feemarket: 'ethermint/feemarket/v1',
+  Types: 'ethermint/types/v1',
 };
 
 export const whitelistIbcExports: { [name: string]: string[] } = {
-  'Controller': ['ibc/applications/interchain_accounts/controller/v1'],
-  'Host': ['ibc/applications/interchain_accounts/host/v1'],
-  'InterchainTypes': ['ibc/applications/interchain_accounts/v1'],
-  'TranferV1': ['ibc/applications/transfer/v1'],
-  'TransferV2': ['ibc/applications/transfer/v2'],
-  'Channel': ['ibc/core/channel/v1'],
-  'Client': ['ibc/core/client/v1'],
-  'Commitment': ['ibc/core/commitment/v1'],
-  'Connection': ['ibc/core/connection/v1'],
-  'LocalHost': ['ibc/lightclients/localhost/v2'],
-  'SolomachineV2': ['ibc/lightclients/solomachine/v2'],
-  'SolomachineV3': ['ibc/lightclients/solomachine/v3'],
-  'Tendermint': ['ibc/lightclients/tendermint/v1'],
+  Controller: ['ibc/applications/interchain_accounts/controller/v1'],
+  Host: ['ibc/applications/interchain_accounts/host/v1'],
+  InterchainTypes: ['ibc/applications/interchain_accounts/v1'],
+  TranferV1: ['ibc/applications/transfer/v1'],
+  TransferV2: ['ibc/applications/transfer/v2'],
+  Channel: ['ibc/core/channel/v1'],
+  Client: ['ibc/core/client/v1'],
+  Commitment: ['ibc/core/commitment/v1'],
+  Connection: ['ibc/core/connection/v1'],
+  LocalHost: ['ibc/lightclients/localhost/v2'],
+  SolomachineV2: ['ibc/lightclients/solomachine/v2'],
+  SolomachineV3: ['ibc/lightclients/solomachine/v3'],
+  Tendermint: ['ibc/lightclients/tendermint/v1'],
 };
