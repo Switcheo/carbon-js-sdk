5670cacf46c6e94b251b8c8f784a3851ad67e475
PC <spclight@gmail.com>
2024-03-12 18:18:47 +0800

    remove: Comment

--- src/constant/gov.ts
+++ src/constant/gov.ts
@@ -2,7 +2,6 @@ import { Network } from "./network";
 import { SWTHAddress } from "@carbon-sdk/util/address";
 
 export const GovModuleAddress = {
-  // [Network.MainNet]: "swth10d07y265gmmuvt4z0w9aw880jnsr700j9uen92",
   [Network.MainNet]: SWTHAddress.getModuleAddress("gov", Network.MainNet),
   [Network.TestNet]: SWTHAddress.getModuleAddress("gov", Network.TestNet),
   [Network.DevNet]: SWTHAddress.getModuleAddress("gov", Network.DevNet),
