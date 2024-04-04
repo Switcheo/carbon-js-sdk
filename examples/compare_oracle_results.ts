ab2192ee5edf23f41c7335291dd775d6586c38eb
yan-soon <yan@Yans-MacBook-Pro.local>
2024-04-04 16:34:44 +0800

    update oracle script

--- examples/compare_oracle_results.ts
+++ examples/compare_oracle_results.ts
@@ -9,36 +9,38 @@ function fetch(url: string, init?: RequestInit): Promise<Response> {
 }
 
 interface SimpleMap<T = unknown> {
-    [index: string]: T;
+  [index: string]: T;
 }
 
 (async () => {
-    const TESTNET_ORACLE_URL = "https://test-api.carbon.network/carbon/oracle/v1/results_latest"
-    const MAINNET_ORACLE_URL = "https://api.carbon.network/carbon/oracle/v1/results_latest"
+  const TESTNET_ORACLE_URL = "https://test-api.carbon.network/carbon/oracle/v1/results_latest"
+  const MAINNET_ORACLE_URL = "https://api.carbon.network/carbon/oracle/v1/results_latest"
 
-    // make get request to testnet oracle api and mainnet and compare
-    const testnetOracles = await fetch(TESTNET_ORACLE_URL).then((res) => res.json());
-    const mainnetOracles = await fetch(MAINNET_ORACLE_URL).then((res) => res.json());
+  // make get request to testnet oracle api and mainnet and compare
+  const testnetOracles = await fetch(TESTNET_ORACLE_URL).then((res) => res.json());
+  const mainnetOracles = await fetch(MAINNET_ORACLE_URL).then((res) => res.json());
 
-    // convert testnet oracles to map and compare
-    const testnetMap : SimpleMap<string> = {}
-    
-    for (const oracle of testnetOracles.results) {
-        const oracleId : string = oracle.oracle_id
-        const price : string = oracle.data
-        testnetMap[oracleId] = price
+  // convert testnet oracles to map and compare
+  const testnetMap: SimpleMap<string> = {}
+
+  for (const oracle of testnetOracles.results) {
+    const oracleId: string = oracle.oracle_id
+    const price: string = oracle.data
+    testnetMap[oracleId] = price
+  }
+
+  for (const oracle of mainnetOracles.results) {
+    const oracleId: string = oracle.oracle_id
+    const price: string = oracle.data
+    const testnetPrice = testnetMap[oracleId]
+    if (!testnetPrice) {
+      console.log(`WARNING! MISSING ORACLE FROM TESTNET: ${oracleId} \n`)
+      continue
     }
-
-    for (const oracle of mainnetOracles.results) {
-        const oracleId : string = oracle.oracle_id
-        const price : string = oracle.data
-        const testnetPrice = testnetMap[oracleId]
-        if (!testnetPrice) {
-            console.log(`WARNING! MISSING ORACLE FROM TESTNET: ${oracleId} \n`)
-            continue
-        }
-        // check if price diff between mainnet and testnet oracles differ by more than 1%
-        const priceDiff = parseFloat(price) / parseFloat(testnetPrice)
-        console.log(`${oracleId}: ${price}, ${testnetPrice}, diff: ${priceDiff} \n`)
+    // check if price diff between mainnet and testnet oracles differ by more than 1%
+    const priceDiff = parseFloat(price) / parseFloat(testnetPrice)
+    if (priceDiff > parseFloat("1.01") || priceDiff < parseFloat("0.99")) {
+      console.log(`${oracleId}: ${price}, ${testnetPrice}, diff: ${priceDiff} \n`)
     }
-  })().catch(console.error).finally(() => process.exit(0));
+  }
+})().catch(console.error).finally(() => process.exit(0));
--- examples/update_oracle_spec.ts
+++ examples/update_oracle_spec.ts
@@ -8,154 +8,183 @@ import path from 'path';
 // Function to read JSON files from a folder
 function readJsonFilesFromFolder(folderPath: string): Promise<{ [fileName: string]: any }> {
   return new Promise((resolve, reject) => {
-      fs.readdir(folderPath, (err, files) => {
-          if (err) {
-              reject(err);
+    fs.readdir(folderPath, (err, files) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      const jsonFiles = files.filter(file => file.endsWith('.json'));
+      const promises = jsonFiles.map(file => {
+        const fileNameWithoutExtension = path.parse(file).name;
+        const filePath = path.join(folderPath, file);
+        return new Promise<{ [fileName: string]: any }>((resolveFile, rejectFile) => {
+          fs.readFile(filePath, 'utf8', (err, data) => {
+            if (err) {
+              rejectFile(err);
               return;
-          }
-
-          const jsonFiles = files.filter(file => file.endsWith('.json'));
-          const promises = jsonFiles.map(file => {
-              const fileNameWithoutExtension = path.parse(file).name;
-              const filePath = path.join(folderPath, file);
-              return new Promise<{ [fileName: string]: any }>((resolveFile, rejectFile) => {
-                  fs.readFile(filePath, 'utf8', (err, data) => {
-                      if (err) {
-                          rejectFile(err);
-                          return;
-                      }
-                      try {
-                          const jsonData = data;
-                          resolveFile({ [fileNameWithoutExtension]: jsonData });
-                      } catch (error) {
-                          rejectFile(error);
-                      }
-                  });
-              });
+            }
+            try {
+              const jsonData = data;
+              resolveFile({ [fileNameWithoutExtension]: jsonData });
+            } catch (error) {
+              rejectFile(error);
+            }
           });
-
-          Promise.all(promises)
-              .then(results => {
-                  const jsonMap: { [fileName: string]: any } = {};
-                  for (const result of results) {
-                      Object.assign(jsonMap, result);
-                  }
-                  resolve(jsonMap);
-              })
-              .catch(reject);
+        });
       });
+
+      Promise.all(promises)
+        .then(results => {
+          const jsonMap: { [fileName: string]: any } = {};
+          for (const result of results) {
+            Object.assign(jsonMap, result);
+          }
+          resolve(jsonMap);
+        })
+        .catch(reject);
+    });
   });
 }
 
 
 (async () => {
   const mnemonics = process.env.MNEMONICS ?? BIP39.generateMnemonic();
-    console.log("mnemonics", mnemonics);
-  
-    const sdk = await CarbonSDK.instance({
-      network: CarbonSDK.Network.TestNet,
-    });
-    const connectedSDK = await sdk.connectWithMnemonic(mnemonics);
-    console.log("connected sdk");
+  console.log("mnemonics", mnemonics);
 
-    const mainnetOracles = await readJsonFilesFromFolder("./examples/mainnet-oracles")
+  const sdk = await CarbonSDK.instance({
+    network: CarbonSDK.Network.MainNet,
+  });
+  const connectedSDK = await sdk.connectWithMnemonic(mnemonics);
+  console.log("connected sdk");
 
-    // const oraclesToUpdate = [
-    //   {
-    //     id: ".CAXLUSDC",
-    //     maxResultAge: 305
-    //   },
-    //   {
-    //     id: ".CEVMOS",
-    //     maxResultAge: 305
-    //   },
-    //   {
-    //     id: ".CIBX",
-    //     maxResultAge: 305
-    //   },
-    //   {
-    //     id: ".CIRIS",
-    //     maxResultAge: 305
-    //   },
-    //   {
-    //     id: ".CKUJI",
-    //     maxResultAge: 320
-    //   },
-    //   {
-    //     id: ".CMILKTIA",
-    //     maxResultAge: 354
-    //   },
-    //   {
-    //     id: ".COSMO",
-    //     maxResultAge: 300
-    //   },
-    //   {
-    //     id: ".CSCRT",
-    //     maxResultAge: 300
-    //   },
-    //   {
-    //     id: ".CSTATOM",
-    //     maxResultAge: 310
-    //   },
-    //   {
-    //     id: ".CSTDYDX",
-    //     maxResultAge: 310
-    //   },
-    //   {
-    //     id: ".CSTEVMOS",
-    //     maxResultAge: 320
-    //   },
-    //   {
-    //     id: ".CSTOSMO",
-    //     maxResultAge: 330
-    //   },
-    //   {
-    //     id: ".CSTRD",
-    //     maxResultAge: 330
-    //   },
-    //   {
-    //     id: ".CSTSTARS",
-    //     maxResultAge: 310
-    //   },
-    //   {
-    //     id: ".CSTTIA",
-    //     maxResultAge: 312
-    //   },
-    // ]
+  const mainnetOracles = await readJsonFilesFromFolder("./examples/mainnet-oracles")
 
-    const oraclesToUpdate = [
-      {
-        id: ".CYIELDUSD",
-        maxResultAge: 482,
-      },
-      {
-        id: "SIDXBTC",
-        maxResultAge: 300,
-      },
-      {
-        id: "SIDXETH",
-        maxResultAge: 300,
-      },
-    ]
-      
-    const txs: any[] = []
-    for (const oracle of oraclesToUpdate) {
-      const spec = mainnetOracles[oracle.id] as string
-      const txUpdateSpec = {
-        typeUrl: CarbonTx.Types.MsgUpdateOracle,
-        value: MsgUpdateOracle.fromPartial({
-          updater: connectedSDK.wallet.bech32Address,
-          updateOracleParams: {
-            id: oracle.id,
-            spec,
-            maxResultAge: oracle.maxResultAge
-          },
-        }),
-      }
-      console.log(`updating oracle ${oracle.id}`)
-      txs.push(txUpdateSpec)
+  // const oraclesToUpdate = [
+  //   {
+  //     id: ".CAXLUSDC",
+  //     maxResultAge: 305
+  //   },
+  //   {
+  //     id: ".CEVMOS",
+  //     maxResultAge: 305
+  //   },
+  //   {
+  //     id: ".CIBX",
+  //     maxResultAge: 305
+  //   },
+  //   {
+  //     id: ".CIRIS",
+  //     maxResultAge: 305
+  //   },
+  //   {
+  //     id: ".CKUJI",
+  //     maxResultAge: 320
+  //   },
+  //   {
+  //     id: ".CMILKTIA",
+  //     maxResultAge: 354
+  //   },
+  //   {
+  //     id: ".COSMO",
+  //     maxResultAge: 300
+  //   },
+  //   {
+  //     id: ".CSCRT",
+  //     maxResultAge: 300
+  //   },
+  //   {
+  //     id: ".CSTATOM",
+  //     maxResultAge: 310
+  //   },
+  //   {
+  //     id: ".CSTDYDX",
+  //     maxResultAge: 310
+  //   },
+  //   {
+  //     id: ".CSTEVMOS",
+  //     maxResultAge: 320
+  //   },
+  //   {
+  //     id: ".CSTOSMO",
+  //     maxResultAge: 330
+  //   },
+  //   {
+  //     id: ".CSTRD",
+  //     maxResultAge: 330
+  //   },
+  //   {
+  //     id: ".CSTSTARS",
+  //     maxResultAge: 310
+  //   },
+  //   {
+  //     id: ".CSTTIA",
+  //     maxResultAge: 312
+  //   },
+  // ]
+
+  const oraclesToUpdate = [
+    {
+      id: ".COSMO",
+      resolution: 5,
+      maxResultAge: 300,
+    },
+    {
+      id: ".CSCRT",
+      resolution: 5,
+      maxResultAge: 300,
+    },
+    {
+      id: ".CNEO",
+      resolution: 5,
+      maxResultAge: 300,
+    },
+    {
+      id: ".CSTX",
+      resolution: 2,
+      maxResultAge: 300,
+    },
+    {
+      id: ".CZIL",
+      resolution: 5,
+      maxResultAge: 300,
+    },
+    {
+      id: ".CBNB",
+      resolution: 5,
+      maxResultAge: 300,
+    },
+    {
+      id: ".CMEME",
+      resolution: 2,
+      maxResultAge: 300,
+    },
+    {
+      id: ".CBLUR",
+      resolution: 2,
+      maxResultAge: 300,
+    },
+  ]
+
+  const txs: any[] = []
+  for (const oracle of oraclesToUpdate) {
+    const spec = mainnetOracles[oracle.id] as string
+    const txUpdateSpec = {
+      typeUrl: CarbonTx.Types.MsgUpdateOracle,
+      value: MsgUpdateOracle.fromPartial({
+        updater: connectedSDK.wallet.bech32Address,
+        updateOracleParams: {
+          id: oracle.id,
+          spec,
+          resolution: oracle.resolution,
+          maxResultAge: oracle.maxResultAge
+        },
+      }),
     }
+    console.log(`updating oracle ${oracle.id}`)
+    txs.push(txUpdateSpec)
+  }
 
-    const result = await connectedSDK.wallet.sendTxs(txs);
-    console.log(result);
-  })().catch(console.error).finally(() => process.exit(0));
+  const result = await connectedSDK.wallet.sendTxs(txs);
+  console.log(result);
+})().catch(console.error).finally(() => process.exit(0));
