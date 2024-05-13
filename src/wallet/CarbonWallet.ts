88af050ec5335ee23dbf8adc74b89c518c1fc956
Andrew Soon <andrewsoon.tx@gmail.com>
2024-05-13 16:41:39 +0800

    Remove extra code

--- src/wallet/CarbonWallet.ts
+++ src/wallet/CarbonWallet.ts
@@ -894,8 +894,8 @@ export class CarbonWallet {
 
   private async getAccount(queryAddress: string, retryCount: number = 0): Promise<QueryAccountInfo | undefined> {
     try {
-      const account = (await this.getQueryClient().auth.Account({ address: queryAddress }))
-      if (account && account.account) {
+      const account = await this.getQueryClient().auth.Account({ address: queryAddress })
+      if (account?.account) {
         const { accountNumber, sequence, address } = BaseAccount.decode(account.account.value)
         return {
           address,
