b2ccb46c1e1419b0559b9f4f48b10618408c72ae
wr1159 <56021409+wr1159@users.noreply.github.com>
2024-06-13 14:50:00 +0800

    Use principal instead of principal_debt in websocket

--- src/websocket/models.ts
+++ src/websocket/models.ts
@@ -275,7 +275,7 @@ export interface Collateral {
 export interface Debt {
   // CDP
   denom: string;
-  principal_debt: string; // string representation of number
+  principal: string; // string representation of number
   initial_cumulative_interest_multiplier: string; // string representation of number
 }
 
@@ -288,7 +288,7 @@ export interface AccountData {
 }
 
 export interface CDPAccountStablecoin {
-  principal_debt: string; // string representation of number
+  principal: string; // string representation of number
   interest_debt: string; // string representation of number
   initial_cumulative_interest_multiplier: string; // string representation of number
 }
