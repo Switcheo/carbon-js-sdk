57edb5b782575e98c9dd741441f122a27a927b14
kylehoang92 <kyle.hoang@techbank-software.com>
2024-06-28 16:43:19 +0700

    fix: missing export types

--- src/provider/index.ts
+++ src/provider/index.ts
@@ -4,6 +4,7 @@ export { ChainInfo, default as KeplrAccount, Keplr, KeplrWindow } from "./keplr"
 export { default as LeapAccount, Leap, Key, LeapExtended } from "./leap";
 export { default as CosmosLedger } from "./ledger";
 export { CallContractArgs, MetaMask } from "./metamask";
+export { EVMChain, SyncResult as MetaMaskSyncResult } from "../constant";
 export { O3Types, O3Wallet } from "./o3";
 export { default as SDKProvider } from "./sdk";
 export { Zilpay, ZilPayChangeNetworkParam } from "./zilpay";
