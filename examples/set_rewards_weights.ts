044c7678230e88ff7864f3b2e5850ab5955673ff
yan-soon <soonxiangyan@gmail.com>
2024-07-24 16:37:19 +0200

    add more mainnet oracles

--- examples/set_rewards_weights.ts
+++ examples/set_rewards_weights.ts
@@ -7,17 +7,39 @@ import "./_setup";
   console.log("mnemonics", mnemonics);
 
   const sdk = await CarbonSDK.instance({
-    network: CarbonSDK.Network.LocalHost,
+    network: CarbonSDK.Network.TestNet,
     config: {
       tmRpcUrl: process.env.TRPC_ENDPOINT,
     },
   });
   const connectedSDK = await sdk.connectWithMnemonic(mnemonics);
   console.log("connected sdk");
-  
-  const result = await connectedSDK.admin.setRewardsWeights([{
-	  poolId: 1,
-    weight: 100,
-  }])
+
+  const result = await connectedSDK.admin.setRewardsWeights([
+    {
+      poolId: 1,
+      weight: 10,
+    },
+    {
+      poolId: 2,
+      weight: 10,
+    },
+    {
+      poolId: 3,
+      weight: 10,
+    },
+    {
+      poolId: 4,
+      weight: 10,
+    },
+    {
+      poolId: 5,
+      weight: 10,
+    },
+    {
+      poolId: 6,
+      weight: 10,
+    },
+  ])
   console.log(result)
 })().catch(console.error).finally(() => process.exit(0));
