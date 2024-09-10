911c50070969d13c9337b3a789f94488ee8713c7
kylehoang92 <kyle.hoang@techbank-software.com>
2024-09-10 16:07:24 +0700

    feat: accept quote

--- src/provider/amino/types/otc.ts
+++ src/provider/amino/types/otc.ts
@@ -5,15 +5,22 @@ import { AminoInit, generateAminoType } from "../utils";
 
 const TxTypes: TypeUtils.SimpleMap<string> = {
   MsgCreateRfq: "otc/MsgCreateRfq",
+  MsgAcceptQuote: "otc/MsgAcceptQuote",
 };
 
 const MsgCreateRfq: AminoInit = {
-  aminoType: TxTypes.SupplyAsset,
+  aminoType: TxTypes.MsgCreateRfq,
+  valueMap: {},
+};
+
+const MsgAcceptQuote: AminoInit = {
+  aminoType: TxTypes.MsgAcceptQuote,
   valueMap: {},
 };
 
 const OtcAmino: TypeUtils.SimpleMap<AminoConverter> = {
-  [CarbonTx.Types.MsgSupplyAsset]: generateAminoType(MsgCreateRfq),
+  [CarbonTx.Types.MsgCreateRfq]: generateAminoType(MsgCreateRfq),
+  [CarbonTx.Types.MsgAcceptQuote]: generateAminoType(MsgAcceptQuote),
 };
 
 export default OtcAmino;
