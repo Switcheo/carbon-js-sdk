6f3856ded4c330f4112eb7f25d63c9242cf4ff9a
Kyle Hoang <166352073+kylehoang92@users.noreply.github.com>
2024-10-03 14:31:44 +0700

    feat: otc module  (#531)
    * feat: otc module
    
    * feat: add new ws channels for otc module
    
    * feat: accept quote
    
    * feat: cancel rfq and refactor code
    
    * fix: address reviewer comments
    
    * fix: sign animo message
    
    * chore: remove fallback
    
    * v0.11.23-beta.2
    
    ---------
    
    Co-authored-by: Thong Yuan Yu Sarah <sarah.thong@switcheo.network>

---
+++ src/modules/otc.ts
@@ -0,0 +1,61 @@
+import { MsgAcceptQuote, MsgCancelRfq, MsgCreateRfq } from "@carbon-sdk/codec/Switcheo/carbon/otc/tx";
+import { CarbonTx } from "@carbon-sdk/util";
+import BaseModule from "./base";
+
+export class OTCModule extends BaseModule {
+  public async createRfq(params: OTCModule.CreateRfqParams, opts?: CarbonTx.SignTxOpts) {
+    const wallet = this.getWallet();
+    const { denoms: sellCoins, buyDenom, expiryTime } = params;
+
+    const value = MsgCreateRfq.fromPartial({
+      requester: wallet.bech32Address,
+      sellCoins,
+      buyDenom: buyDenom,
+      expiryTime: expiryTime,
+    })
+
+    return await wallet.sendTx({
+      typeUrl: CarbonTx.Types.MsgCreateRfq,
+      value,
+    }, opts)
+  }
+
+  public async cancelRfq(rfqId: string, opts?: CarbonTx.SignTxOpts) {
+    const wallet = this.getWallet();
+
+    const value = MsgCancelRfq.fromPartial({
+      requester: wallet.bech32Address,
+      id: rfqId,
+    })
+
+    return await wallet.sendTx({
+      typeUrl: CarbonTx.Types.MsgCancelRfq,
+      value,
+    }, opts)
+  }
+
+  public async acceptQuote(quoteId: string, opts?: CarbonTx.SignTxOpts) {
+    const wallet = this.getWallet();
+
+    const value = MsgAcceptQuote.fromPartial({
+      requester: wallet.bech32Address,
+      id: quoteId,
+    })
+
+    return await wallet.sendTx({
+      typeUrl: CarbonTx.Types.MsgAcceptQuote,
+      value,
+    }, opts)
+  }
+}
+
+export namespace OTCModule {
+  export interface CreateRfqParams {
+    denoms: {
+      denom: string;
+      amount: string;
+    }[],
+    buyDenom: string
+    expiryTime: Date
+  }
+}
