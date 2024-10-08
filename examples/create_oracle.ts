3579a84f4855a955a2cddb504cdbfb0bd7607133
yan-soon <soonxiangyan@gmail.com>
2024-10-08 11:21:30 +0800

    add eigen oracle

--- examples/create_oracle.ts
+++ examples/create_oracle.ts
@@ -1,29 +1,92 @@
 import * as BIP39 from "bip39";
-import { CarbonSDK } from "./_sdk";
+import { CarbonSDK, CarbonTx } from "./_sdk";
 import "./_setup";
+import { MsgCreateOracle } from "../lib/codec/Switcheo/carbon/oracle/tx";
+import fs from 'fs';
+import path from 'path';
+
+// Function to read JSON files from a folder
+function readJsonFilesFromFolder(folderPath: string): Promise<{ [fileName: string]: any }> {
+  return new Promise((resolve, reject) => {
+    fs.readdir(folderPath, (err, files) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      const jsonFiles = files.filter(file => file.endsWith('.json'));
+      const promises = jsonFiles.map(file => {
+        const fileNameWithoutExtension = path.parse(file).name;
+        const filePath = path.join(folderPath, file);
+        return new Promise<{ [fileName: string]: any }>((resolveFile, rejectFile) => {
+          fs.readFile(filePath, 'utf8', (err, data) => {
+            if (err) {
+              rejectFile(err);
+              return;
+            }
+            try {
+              const jsonData = data;
+              resolveFile({ [fileNameWithoutExtension]: jsonData });
+            } catch (error) {
+              rejectFile(error);
+            }
+          });
+        });
+      });
+
+      Promise.all(promises)
+        .then(results => {
+          const jsonMap: { [fileName: string]: any } = {};
+          for (const result of results) {
+            Object.assign(jsonMap, result);
+          }
+          resolve(jsonMap);
+        })
+        .catch(reject);
+    });
+  });
+}
+
 
 (async () => {
   const mnemonics = process.env.MNEMONICS ?? BIP39.generateMnemonic();
   console.log("mnemonics", mnemonics);
 
   const sdk = await CarbonSDK.instance({
-    network: CarbonSDK.Network.LocalHost,
-    config: {
-      tmRpcUrl: process.env.TRPC_ENDPOINT,
-    },
+    network: CarbonSDK.Network.MainNet,
   });
   const connectedSDK = await sdk.connectWithMnemonic(mnemonics);
   console.log("connected sdk");
 
-  const result = await connectedSDK.admin.createOracle({
-    id: "DXBT2",
-    description: "Demex XBT Index",
-    minTurnoutPercentage: 67,
-    maxResultAge: 100,
-    securityType: "SecuredByValidators",
-    resultStrategy: "median",
-    resolution: 1,
-    spec: "{}",
-  })
-  console.log(result)
+  const mainnetOraclesMap = await readJsonFilesFromFolder("./examples/mainnet-oracles")
+
+  const oraclesToCreate = [
+    ".CEIGEN"
+  ]
+
+  const txs: any[] = []
+  for (const oracleId of oraclesToCreate) {
+    const spec = mainnetOraclesMap[oracleId] as string
+    const txCreateOracle = {
+      typeUrl: CarbonTx.Types.MsgCreateOracle,
+      value: MsgCreateOracle.fromPartial({
+        creator: connectedSDK.wallet.bech32Address,
+        createOracleParams: {
+          id: oracleId,
+          description: `Carbon ${oracleId} Index`,
+          minTurnoutPercentage: 67,
+          maxResultAge: 300,
+          securityType: "SecuredByValidators",
+          resultStrategy: "median",
+          resolution: 2,
+          spec,
+        },
+      }),
+    }
+    console.log(`creating oracle ${oracleId}`)
+    txs.push(txCreateOracle)
+  }
+
+  const result = await connectedSDK.wallet.sendTxs(txs);
+  console.log(result);
 })().catch(console.error).finally(() => process.exit(0));
