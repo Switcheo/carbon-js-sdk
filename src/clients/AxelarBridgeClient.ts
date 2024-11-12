9ae9f948d125b46d0e9931e93b1fb25980732d28
Andrew Soon <77280201+andrewsoon@users.noreply.github.com>
2024-11-12 11:13:35 +0800

    Add gasPrice override to axelarBridge deposit (#543)
    * Add gasPrice override to axelarBridge deposit
    
    * v0.11.29-beta.1
    
    * Update abi file
    
    * Update deposit method in axelar bridge
    
    * Fix naming and prettier
    
    * v0.11.29-beta.2
    
    ---------
    
    Co-authored-by: Thong Yuan Yu Sarah <sarah.thong@switcheo.network>
    Co-authored-by: Minh Đức Đào <minhducdao@Minhs-MacBook-Pro.local>

--- src/clients/AxelarBridgeClient.ts
+++ src/clients/AxelarBridgeClient.ts
@@ -12,12 +12,13 @@ export interface DepositParams {
   senderAddress: string;
   receiverAddress: string;
   amount: BigNumber;
-  depositTokenExternalAddress: string;
+  depositTokenExternalAddress?: string;
   rpcUrl: string;
   gasPriceGwei?: BigNumber;
   gasLimit?: BigNumber;
   signer: ethers.Signer;
-  nonce?: number
+  nonce?: number;
+  isNativeTokenDeposit?: boolean;
 }
 
 export interface EthersTransactionResponse extends ethers.Transaction {
@@ -25,29 +26,55 @@ export interface EthersTransactionResponse extends ethers.Transaction {
 }
 
 export class AxelarBridgeClient {
-
-  private constructor(
-    public readonly configProvider: NetworkConfigProvider,
-  ) { }
+  private constructor(public readonly configProvider: NetworkConfigProvider) {}
 
   public static instance(opts: AxelarBridgeClientOpts) {
-    const { configProvider } = opts
-    return new AxelarBridgeClient(configProvider)
+    const { configProvider } = opts;
+    return new AxelarBridgeClient(configProvider);
   }
 
-  // lock deposit 
+  // lock deposit
   public async deposit(params: DepositParams): Promise<EthersTransactionResponse> {
-    const { contractAddress, senderAddress, receiverAddress, depositTokenExternalAddress, amount, signer, rpcUrl } = params;
-    const rpcProvider = new ethers.providers.JsonRpcProvider(rpcUrl)
-    const contract = new ethers.Contract(contractAddress, ABIs.axelarBridge, rpcProvider)
+    const {
+      contractAddress,
+      senderAddress,
+      receiverAddress,
+      depositTokenExternalAddress,
+      amount,
+      signer,
+      rpcUrl,
+      nonce,
+      gasPriceGwei,
+      gasLimit,
+      isNativeTokenDeposit = false,
+    } = params;
+    const rpcProvider = new ethers.providers.JsonRpcProvider(rpcUrl);
+    const abi = isNativeTokenDeposit ? ABIs.nativeDepositer : ABIs.axelarBridge;
+    const contract = new ethers.Contract(contractAddress, abi, rpcProvider);
+
+    const txParams = {
+      nonce,
+      ...(gasPriceGwei && { gasPrice: gasPriceGwei.shiftedBy(9).toString(10) }),
+      ...(gasLimit && { gasLimit: gasLimit.toString(10) }),
+      ...(isNativeTokenDeposit && { value: amount.toString(10) }),
+    };
+
+    if (isNativeTokenDeposit) {
+      return await contract.connect(signer).deposit(
+        senderAddress, // tokenSender
+        receiverAddress, // carbonReceiver bech32Address
+        txParams
+      );
+    }
 
     return await contract.connect(signer).deposit(
       senderAddress, // tokenSender
       receiverAddress, // carbonReceiver bech32Address
       depositTokenExternalAddress, // asset
       amount.toString(10),
-    )
+      txParams
+    );
   }
 }
 
-export default AxelarBridgeClient
+export default AxelarBridgeClient;
