28ea837e82d0d455238a4286105cdb8e653fe7cd
Andrew Soon <77280201+andrewsoon@users.noreply.github.com>
2024-11-14 14:40:49 +0800

    Update t&c message (#549)

--- src/util/evm.ts
+++ src/util/evm.ts
@@ -1,6 +1,6 @@
 import { ethers } from 'ethers'
 
-export const DEFAULT_PUBLIC_KEY_MESSAGE = `By signing, I confirm that I have read and agreed to the terms and conditions outlined here (https://guide.dem.exchange/technical/terms-and-conditions).\nAdditionally, I verify that I am not a citizen of any of the following countries: Afghanistan, Angola, Central African Republic, China (Mainland), Côte d'Ivoire, Crimea, Cuba, Democratic Republic of Congo, Ethiopia, Guinea-Bissau, Haiti, Iran, Kuwait, Lebanon, Liberia, Libya, Mali, North Korea, Rwanda, Sevastopol, Sierra Leone, Singapore, Somalia, South Africa, Sudan, South Sudan, Syria, Quebec (Canada), U.S, Yemen, Zimbabwe.`
+export const DEFAULT_PUBLIC_KEY_MESSAGE = `By signing this message, I declare that I am the owner of this wallet and that I have read and agreed to the Terms & Conditions (T&Cs) of using Demex found here: https://guide.dem.exchange/technical/terms-and-conditions.\nI further confirm that I do not reside in the Restricted Locations listed in Section 2 of the T&Cs, or any other region where using Demex may not be permitted.`
 
 export const recoverPublicKey = (message: string, signature: string): string => {
   const uncompressedPublicKey = ethers.utils.recoverPublicKey(ethers.utils.hashMessage(message), signature)
