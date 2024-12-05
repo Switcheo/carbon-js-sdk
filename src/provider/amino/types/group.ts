e7687f2a704a0e4ef8aa46ba79a90ab7c10b88d1
tehkohrz <dongkunkoh@gmail.com>
2024-12-05 11:00:32 +0800

    feat: add vote and exec amino types

--- src/provider/amino/types/group.ts
+++ src/provider/amino/types/group.ts
@@ -1,10 +1,12 @@
 import { TypeUtils } from "@carbon-sdk/util";
 import * as CarbonTx from "@carbon-sdk/util/tx";
 import { AminoConverter } from "@cosmjs/stargate";
-import { AminoInit, generateAminoType } from "../utils";
+import { AminoInit, ConvertEncType, generateAminoType } from "../utils";
 
 const TxTypes: TypeUtils.SimpleMap<string> = {
   SubmitGroupProposal: "cosmos-sdk/group/MsgSubmitProposal",
+  VoteProposal: "cosmos-sdk/group/MsgVote",
+  ExecProposal: "cosmos-sdk/group/MsgExec",
 };
 
 const MsgSubmitGroupProposal: AminoInit = {
@@ -12,8 +14,24 @@ const MsgSubmitGroupProposal: AminoInit = {
   valueMap: {},
 };
 
+const MsgVoteGroupProposal: AminoInit = {
+  aminoType: TxTypes.VoteProposal,
+  valueMap: {
+    proposalId: ConvertEncType.Long,
+  },
+};
+
+const MsgExecGroupProposal: AminoInit = {
+  aminoType: TxTypes.ExecProposal,
+  valueMap: {
+    proposalId: ConvertEncType.Long,
+  },
+};
+
 const GroupAmino: TypeUtils.SimpleMap<AminoConverter> = {
   [CarbonTx.Types.MsgGroupSubmitProposal]: generateAminoType(MsgSubmitGroupProposal),
+  [CarbonTx.Types.MsgGroupVote]: generateAminoType(MsgVoteGroupProposal),
+  [CarbonTx.Types.MsgGroupExec]: generateAminoType(MsgExecGroupProposal),
 };
 
 export default GroupAmino;
