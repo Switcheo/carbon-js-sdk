8c64164191faa46cd7e3d7a37aeb8a9c8af753e8
Andrew Soon <andrewsoon.tx@gmail.com>
2024-12-10 15:31:52 +0800

    Fix default metamask provider

--- src/provider/metamask/MetaMask.ts
+++ src/provider/metamask/MetaMask.ts
@@ -2,7 +2,7 @@ import { TxTypes, registry } from "@carbon-sdk/codec";
 import { AuthInfo } from "@carbon-sdk/codec/cosmos/tx/v1beta1/tx";
 import { BASE_MAINNET, BASE_TESTNET, CarbonEvmChainIDs, EthNetworkConfig, MANTLE_MAINNET, MANTLE_TESTNET, Network, NetworkConfigs, OP_MAINNET, OP_TESTNET, RequestArguments, SyncResult } from "@carbon-sdk/constant";
 import { ABIs } from "@carbon-sdk/eth";
-import { AminoTypesMap, AuthUtils, CarbonSDK, EvmUtils, Models, ProviderAgent } from "@carbon-sdk/index";
+import { AminoTypesMap, AuthUtils, CarbonSDK, EvmUtils, Models, ProviderAgent, SupportedEip6963Provider } from "@carbon-sdk/index";
 import { AddressUtils, CarbonTx } from "@carbon-sdk/util";
 import { ETHAddress, SWTHAddress, SWTHAddressOptions } from "@carbon-sdk/util/address";
 import { BLOCKCHAIN_V2_TO_V1_MAPPING, BlockchainV2, ChainNames, EVMChain, getBlockchainFromChainV2 } from "@carbon-sdk/util/blockchain";
@@ -415,12 +415,11 @@ export class MetaMask extends Eip6963Provider {
   }
 
   getDefaultMetamaskProvider(): MetaMaskAPI | undefined {
-    const metamaskProvider = super.getDefaultProvider()
+    const metamaskProvider = super.getProvider(SupportedEip6963Provider.MetaMask)
     return metamaskProvider?.provider as MetaMaskAPI
   }
 
   async getConnectedAPI(): Promise<MetaMaskAPI> {
-    
     const metamaskProvider = this.getDefaultMetamaskProvider()
 
     if (!metamaskProvider) {
