7e3d535eb0a44e31934055d97f5f4277ed25eda4
Randy <randy75828@gmail.com>
2024-12-30 20:46:35 +0800

    try with base64 encoding

--- src/provider/amino/types/bridge.ts
+++ src/provider/amino/types/bridge.ts
@@ -1,7 +1,8 @@
 import { TypeUtils } from "@carbon-sdk/util";
 import * as CarbonTx from "@carbon-sdk/util/tx";
 import { AminoConverter } from "@cosmjs/stargate";
-import { AminoInit, ConvertEncType, generateAminoType } from "../utils";
+import { AminoInit, AminoProcess, AminoValueMap, ConvertEncType, generateAminoType } from "../utils";
+import { MsgExecuteFromCarbon } from "@carbon-sdk/codec/Switcheo/carbon/bridge/tx";
 
 
 const TxTypes: TypeUtils.SimpleMap<string> = {
@@ -22,10 +23,33 @@ const MsgWithdrawAndExecuteToken: AminoInit = {
   },
 };
 
+const withdrawAndExecuteAminoProcess: AminoProcess = {
+  toAminoProcess: (amino: AminoValueMap, input: any) => {
+    const { executionBytes } = input as MsgExecuteFromCarbon;
+    return {
+      amino,
+      input: {
+        ...input,
+        executionBytes: Buffer.from(executionBytes).toString('base64'),
+      },
+    };
+  },
+  fromAminoProcess: (amino: AminoValueMap, input: any) => {
+    const buffer = Buffer.from(input.executionBytes, 'base64');
+    return {
+      amino,
+      input: {
+        ...input,
+        executionBytes: new Uint8Array(buffer),
+      },
+    };
+  },
+};
+
 
 const BridgeAmino: TypeUtils.SimpleMap<AminoConverter> = {
   [CarbonTx.Types.MsgWithdrawToken]: generateAminoType(MsgWithdrawToken),
-  [CarbonTx.Types.MsgExecuteFromCarbon]: generateAminoType(MsgWithdrawAndExecuteToken),
+  [CarbonTx.Types.MsgExecuteFromCarbon]: generateAminoType(MsgWithdrawAndExecuteToken, withdrawAndExecuteAminoProcess),
 };
 
 export default BridgeAmino;
