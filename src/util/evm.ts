46dfbca69485a43aa6efbea804b8c324eb5fd518
Thong Yuan Yu Sarah <sarah.thong@switcheo.network>
2025-01-27 15:27:02 +0800

    Organise imports

--- src/util/evm.ts
+++ src/util/evm.ts
@@ -1,7 +1,6 @@
-import { ethers } from "ethers"
-
-import { SimpleMap } from "./type";
 import BigNumber from "bignumber.js";
+import { ethers } from "ethers";
+import { SimpleMap } from "./type";
 
 export const DEFAULT_PUBLIC_KEY_MESSAGE = `By signing this message, I declare that I am the owner of this wallet and that I have read and agreed to the Terms & Conditions (T&Cs) of using Demex found here: https://guide.dem.exchange/technical/terms-and-conditions.\nI further confirm that I do not reside in the Restricted Locations listed in Section 2 of the T&Cs, or any other region where using Demex may not be permitted.`;
 
