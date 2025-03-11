1822f8dca2b56dee9fd45d89aa561462a3ed0056
Song Tian <songtian.liu@switcheo.network>
2025-03-11 16:53:22 +0800

    replace hardcoded network config for  with data retrieved from chainid.network API  endpoint

--- src/provider/metamask/MetaMask.ts
+++ src/provider/metamask/MetaMask.ts
@@ -1,6 +1,6 @@
 import { registry, TxTypes } from "@carbon-sdk/codec";
 import { AuthInfo } from "@carbon-sdk/codec/cosmos/tx/v1beta1/tx";
-import { AVALANCHE_MAINNET, AVALANCHE_TESTNET, BASE_MAINNET, BASE_TESTNET, CarbonEvmChainIDs, EthNetworkConfig, MANTLE_MAINNET, MANTLE_TESTNET, Network, NetworkConfigs, OP_MAINNET, OP_TESTNET, RequestArguments, SyncResult } from "@carbon-sdk/constant";
+import { CarbonEvmChainIDs, EthNetworkConfig, getChangeNetworkParams, Network, NetworkConfigs, RequestArguments, SyncResult } from "@carbon-sdk/constant";
 import { ABIs } from "@carbon-sdk/eth";
 import { AminoTypesMap, AuthUtils, CarbonSDK, EvmUtils, Models, ProviderAgent, SupportedEip6963Provider } from "@carbon-sdk/index";
 import { AddressUtils, CarbonTx } from "@carbon-sdk/util";
@@ -22,7 +22,7 @@ import { StdFee } from "@cosmjs/stargate";
 import { TxBody } from "cosmjs-types/cosmos/tx/v1beta1/tx";
 import * as ethSignUtils from "eth-sig-util";
 import { ethers } from "ethers";
-import { ARBITRUM_MAINNET, ARBITRUM_TESTNET, BSC_MAINNET, BSC_TESTNET, CARBON_EVM_DEVNET, CARBON_EVM_LOCALHOST, CARBON_EVM_MAINNET, CARBON_EVM_TESTNET, ChangeNetworkParam as MetaMaskChangeNetworkParam, ETH_MAINNET, ETH_TESTNET, OKC_MAINNET, OKC_TESTNET, POLYGON_MAINNET, POLYGON_TESTNET } from "../../constant";
+import { CARBON_EVM_DEVNET, CARBON_EVM_LOCALHOST, CARBON_EVM_MAINNET, CARBON_EVM_TESTNET, ChangeNetworkParam as MetaMaskChangeNetworkParam } from "../../constant";
 import { Eip6963Provider } from "../eip6963Provider";
 import { parseEvmError } from "./error";
 import { LEGACY_ACCOUNTS_MAINNET, LEGACY_ACCOUNTS_TESTNET } from "./legacy-accounts";
@@ -314,35 +314,15 @@ export class MetaMask extends Eip6963Provider {
     }
   }
 
-  static getNetworkParams(network: Network, blockchain: EVMChain = 'Ethereum'): MetaMaskChangeNetworkParam {
+  static async getNetworkParams(network: Network, blockchain: EVMChain = 'Ethereum'): Promise<MetaMaskChangeNetworkParam> {
     if (blockchain === 'Carbon') {
       return MetaMask.getCarbonEvmNetworkParams(network)
     }
 
-    const isMainnet = network === Network.MainNet
-
-    switch (blockchain) {
-      case 'Binance Smart Chain':
-        return isMainnet ? BSC_MAINNET : BSC_TESTNET
-      case 'Arbitrum':
-        return isMainnet ? ARBITRUM_MAINNET : ARBITRUM_TESTNET
-      case 'Polygon':
-        return isMainnet ? POLYGON_MAINNET : POLYGON_TESTNET
-      case 'OKC':
-        return isMainnet ? OKC_MAINNET : OKC_TESTNET
-      case 'Mantle':
-        return isMainnet ? MANTLE_MAINNET : MANTLE_TESTNET
-      case 'Optimism':
-        return isMainnet ? OP_MAINNET : OP_TESTNET
-      case 'Avalanche':
-        return isMainnet ? AVALANCHE_MAINNET : AVALANCHE_TESTNET
-      case 'Base':
-        return isMainnet ? BASE_MAINNET : BASE_TESTNET
-      default:
-        // metamask should come with Ethereum configs
-        return isMainnet ? ETH_MAINNET : ETH_TESTNET
-    }
+    const chainId = this.getRequiredChainId(network, blockchain)
+    return await getChangeNetworkParams(chainId)
   }
+
   static getCarbonEvmNetworkParams(network: Network): MetaMaskChangeNetworkParam {
     switch (network) {
       case Network.LocalHost:
@@ -490,9 +470,10 @@ export class MetaMask extends Eip6963Provider {
     } catch (err) {
       // This error code indicates that the chain has not been added to MetaMask.
       try {
+        const networkParams = await MetaMask.getNetworkParams(network, blockchain)
         await metamaskApi.request({
           method: 'wallet_addEthereumChain',
-          params: [MetaMask.getNetworkParams(network, blockchain)],
+          params: [networkParams],
         });
         await this.syncBlockchain();
       } catch (err) {
