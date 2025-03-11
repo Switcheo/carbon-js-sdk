bd3cf87320fc34a58e0a9ef0fe44eaaca359bf60
Koh Dong Kun <dongkunkoh@gmail.com>
2025-03-11 15:07:21 +0800

    feat: gen codec for v2.65.0 (#597)

--- src/codec/Switcheo/carbon/book/export.ts
+++ src/codec/Switcheo/carbon/book/export.ts
@@ -1,4 +1,4 @@
 export { OrderBookLevel, OrderBook, StopBook, StopOrder } from "./book"
-export { OrderBookEvent, VirtualOrderBookEvent, ClearVirtualOrderBookEvent } from "./event"
+export { OrderBookEvent, Quote, VirtualOrderBookEvent, ClearVirtualOrderBookEvent } from "./event"
 export { StopbookV2320 } from "./legacy"
 export { QueryImpactPriceRequest, QueryImpactPriceResponse, QueryGetBookRequest, QueryGetBookResponse, QueryAllBookRequest, QueryAllBookResponse, QueryGetStopBookRequest, QueryGetStopBookResponse, QueryAllStopBookRequest, QueryAllStopBookResponse } from "./query"
