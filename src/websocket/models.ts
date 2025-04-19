0a6004b2dcacf4a5c8ff0280b4adde5362e057a2
Thong Yuan Yu Sarah <sarah.thong@switcheo.network>
2025-04-20 00:05:19 +0800

    Update leverage websocket struct

--- src/websocket/models.ts
+++ src/websocket/models.ts
@@ -149,7 +149,7 @@ export interface PriceLevelEvent extends PriceLevel {
 export interface Leverage {
   market_id?: string;
   leverage: string; // string representation of number
-  is_cross: boolean;
+  is_cross?: boolean;
 }
 
 export interface Pool {
