97ecb31911b3569fc974938634df00f1ff134d66
Randy <randy75828@gmail.com>
2025-04-21 18:04:11 +0800

    add allowance codec

--- scripts/generate-registry.ts
+++ scripts/generate-registry.ts
@@ -20,7 +20,7 @@ const carbonFolders = ['admin', 'bank', 'book', 'bridge', 'broker', 'cdp', 'coin
 console.log(`import { Registry } from "@cosmjs/proto-signing";`);
 // TODO: Remove hardcoded statement when upgrading cosmwasm codecs
 console.log(`import { MsgExecuteContract } from "cosmjs-types/cosmwasm/wasm/v1/tx";`);
-
+console.log(`import { BasicAllowance, PeriodicAllowance, AllowedMsgAllowance } from "cosmjs-types/cosmos/feegrant/v1beta1/feegrant";`);
 const modules: { [name: string]: string[] } = {};
 for (const moduleFile of codecFiles) {
 
@@ -144,6 +144,9 @@ for (const packageName in modules) {
 }
 // TODO: Remove hardcoded statement when upgrading cosmwasm codecs
 console.log("registry.register(\"/cosmwasm.wasm.v1.MsgExecuteContract\", MsgExecuteContract);");
+console.log(`registry.register(\"/cosmos.feegrant.v1beta1.BasicAllowance\", BasicAllowance);`);
+console.log(`registry.register(\"/cosmos.feegrant.v1beta1.PeriodicAllowance\", PeriodicAllowance);`);
+console.log(`registry.register(\"/cosmos.feegrant.v1beta1.AllowedMsgAllowance\", AllowedMsgAllowance);`);
 typeMap.MsgExecuteContract = "/cosmwasm.wasm.v1.MsgExecuteContract";
 
 console.log("");
